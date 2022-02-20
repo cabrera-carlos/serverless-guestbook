c6dfcf14db9fab9cfcb9eca4c8eb658d0a9f48f8
Carlos Cabrera <62525713+cabrera-carlos@users.noreply.github.com>
2022-02-20 12:57:10 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3b7b04da.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
